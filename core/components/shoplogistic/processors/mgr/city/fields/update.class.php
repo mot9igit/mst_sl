198b3efd73a6b93c84e8bb1139184f4f22c731d1
mot9igit <artpetropavlovskij@gmail.com>
2022-12-02 16:23:22 +0500

    initial 2

--- core/components/shoplogistic/processors/mgr/city/fields/update.class.php
+++
@@ -1,6 +0,0 @@
-<?php
-class slCityFieldsUpdateProcessor extends modObjectUpdateProcessor {
-    public $classKey = 'slCityFields';
-}
-
-return "slCityFieldsUpdateProcessor";
--- core/components/shoplogistic/processors/mgr/city/resource/update.class.php
+++
@@ -1,6 +0,0 @@
-<?php
-class slCityResourceRProcessor extends modObjectUpdateProcessor {
-    public $classKey = 'slCityResource';
-}
-
-return "slCityResourceRProcessor";
